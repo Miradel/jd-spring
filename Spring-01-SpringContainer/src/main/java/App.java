b677e320683bd3b472cbe221096472a63d7def1f
Miradel <miradel123@Gmail.com>
2020-10-24 13:03:23 -0400

    Spring Container

--- Spring-01-SpringContainer/src/main/java/App.java
+++ Spring-01-SpringContainer/src/main/java/App.java
@@ -6,11 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
 
     public static void main(String[] args) {
-
+        // Way 1 creating Container
         BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
         Mentor mentor = (Mentor) container.getBean("partTimeMentor");
         mentor.createAccount();
 
+        // Way 2 creating Container
         ApplicationContext container2 = new ClassPathXmlApplicationContext("config.xml");
         Mentor mentor2 = container2.getBean("fullTimeMentor",Mentor.class);
         mentor2.createAccount();
