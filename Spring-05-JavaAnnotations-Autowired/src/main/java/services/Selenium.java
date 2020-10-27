20afe4ce74918bd0c45f3ac91d2be1564c483d7b
Miradel <miradel123@Gmail.com>
2020-10-27 17:31:13 -0400

    JavaAnnotations - Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/services/Selenium.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/services/Selenium.java
@@ -1,26 +1,24 @@
 package services;
 
 import interfaces.Course;
+import interfaces.ExtraSessions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Selenium implements Course {
 
-    private OfficeHours officeHours;
+    private ExtraSessions extraSessions;
 
+    //Setter Injection
     @Autowired
-    public void setOfficeHours(OfficeHours officeHours) {
-        this.officeHours = officeHours;
+    public void setExtraSessions(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
     }
 
-    public OfficeHours getOfficeHours() {
-        return officeHours;
-    }
 
-    // Setter Injection
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours Selenium :" + (20 + officeHours.getHours()));
+        System.out.println("Weekly teaching hours Selenium :" + (20 + extraSessions.getHours()));
     }
 }
