b5c1141572f752ec174279faac57b36cb8464b25
Miradel <miradel123@Gmail.com>
2021-01-19 18:48:46 -0500

    Security - HttpBasic AutRoleBase

---
+++ Spring-10-Security-HttpBasic-AutRoleBase/src/main/java/com/cybertek/entity/User.java
@@ -0,0 +1,9 @@
+package com.cybertek.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+public class User {
+}
